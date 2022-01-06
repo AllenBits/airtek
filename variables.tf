52b61132279c10a05c1bfe8817a965e2dddd121b
femi4allen <femi4allen@gmail.com>
2022-01-05 19:45:44 -0500

    tech assessment

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "profile" {
+  description = "AWS profile"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region to deploy to"
+  default = "us-east-1"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name"
+  type = string
+}
